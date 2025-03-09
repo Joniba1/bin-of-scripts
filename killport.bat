17192e85874503899f2709766d2fb77153c20027
joniba1 <joniba79@gmail.com>
2025-03-09 22:32:30 +0200

    updated killport.bat description

--- killport.bat
+++ killport.bat
@@ -1,9 +1,8 @@
-:: This script terminates the process that is LISTENING on a specified port 
-:: by finding its PID using netstat and then killing it with taskkill
-
+:: Description: Terminates the process that is LISTENING on a specified port by finding its PID using netstat and then killing it with taskkill
 :: Usage: killport [port]
 
 @echo off
+
 if "%1"=="" (
     echo Usage: killport [port]
     exit /b
