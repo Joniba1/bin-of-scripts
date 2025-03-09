d2abe926a5c8f3e3662bfb243a1764e5e09841dd
joniba1 <joniba79@gmail.com>
2025-03-09 23:31:05 +0200

    patched reduce-video.bat

--- scripts/reduce-video.bat
+++ scripts/reduce-video.bat
@@ -1,15 +1,24 @@
 :: Description: Reduces the size of a video by scaling it to 720p and compressing it using H.264 codec. Audio quality is preserved.
 :: Output: Saves the reduced video in the same directory as the input file with a "_reduced" appended to the original path.
-:: Usage: reduce-video.bat [video path]
+:: Usage: reduce-video [video path]
 
 @echo off
-
 if "%~1"=="" (
-    echo Usage: %~nx0 input_file
+    echo Usage: reduce-video input_file
     echo Input file path must be provided.
     exit /b 1
 )
 
 set "INPUT_FILE=%~1"
- 
+set "FILE_DIR=%~dp1"
+set "FILE_NAME=%~n1"
+set "FILE_EXT=%~x1"
+
+set "OUTPUT_FILE=%FILE_DIR%%FILE_NAME%_reduced%FILE_EXT%"
+
+
+echo Processing...
+
 ffmpeg -hide_banner -loglevel error -i "%INPUT_FILE%" -vf scale=1280:720 -c:v libx264 -crf 23 -preset veryslow -c:a copy "%OUTPUT_FILE%"
+
+echo Output file: "%OUTPUT_FILE%"
