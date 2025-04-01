b049277411d54257c1a2b97d527ab02b4d670bca
Jonathan <joniba79@gmail.com>
2025-04-01 14:31:20 +0300

    support multiple ports termination

--- scripts/killport.bat
+++ scripts/killport.bat
@@ -1,19 +1,27 @@
-:: Description: Terminates the process that is LISTENING on a specified port by finding its PID using netstat and then killing it with taskkill
-:: Usage: killport [port]
+:: Description: Terminates processes LISTENING on specified ports by finding their PIDs using netstat and then killing them with taskkill
+:: Usage: killport [port1] [port2] [port3] ...
 
 @echo off
 
 if "%1"=="" (
-    echo Usage: killport [port]
-    exit /b
+  echo Usage: killport [port1] [port2] [port3] ...
+  exit /b
 )
 
 setlocal enabledelayedexpansion
 
+:loop
+if "%1"=="" goto :end
+
 for /f "tokens=5" %%a in ('netstat -ano ^| findstr ":%1" ^| findstr "LISTENING"') do (
-    echo Found LISTENING process on port %1 with PID %%a
-    echo Terminating PID %%a...
-    taskkill /PID %%a /F
-    echo PID %%a terminated successfully.
+  echo Found LISTENING process on port %1 with PID %%a
+  echo Terminating PID %%a...
+  taskkill /PID %%a /F
+  echo PID %%a terminated successfully.
 )
 
+shift
+goto :loop
+
+:end
+endlocal
